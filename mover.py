3f26be57ae5e0342984e93526465e649fa6eaad6
Madalin Fasie <fasie.madalin@gmail.com>
2018-07-01 09:04:54 +0300

    refactor moving files and adding new extensions

--- mover.py
+++ mover.py
@@ -1,6 +1,8 @@
 import shutil
-import os
 import sys
+import os
+from os import path
+from subprocess import check_call
 from PyQt5.QtWidgets import (QApplication,
                              QMainWindow,
                              QAction,
@@ -45,64 +47,74 @@ DOCUMENTS_EXTENSIONS = ('',
                         '.xlsx')
 ZIP_EXTENSIONS = ('.zip',
                   '.tar.gz',
-                  '.rar')
+                  '.rar',
+                  '.gzip',
+                  '.7z')
+
 
 class Mover:
+    """ Moves files from one folder to another """
     def __init__(self, progressbar):
         self.progress = progressbar
+
+    @staticmethod
+    def __flatten_list(list_):
+        """ Flatten a list of lists (only one level) """
+        return [extension for ext_tuple in list_ for extension in ext_tuple]
 
     def run_move_files(self, file_path_from, file_path_to, file_extensions):
         """ The logic for moving the files from one location to another
         It takes as parameters the two locations and a list with the
         extensions for the files that will be moved """
-        duplicate = 1
         n_files = 0
+        remove_zips = False
 
-        # count all the files affected
-        for extensions in file_extensions:
-            for root, dirs, files in os.walk(file_path_from):
-                for file_name in files:
-                    if file_name.endswith(extensions)\
-                        and root == file_path_from:
-                        n_files += 1
+        # If zip option is checked, remove the extensions from list
+        # and mark remove_zips as True
+        if ZIP_EXTENSIONS in file_extensions:
+            remove_zips = True
+            file_extensions.remove(ZIP_EXTENSIONS)
 
-        if n_files > 0:
-            # execute the operation
-            for extensions in file_extensions:
-                for root, dirs, files in os.walk(file_path_from):
-                    for file_name in files:
-                        if file_name.endswith(extensions)\
-                            and root == file_path_from\
-                            and not file_name.endswith(ZIP_EXTENSIONS):
+        # Flatten the list of extensions
+        file_extensions = Mover.__flatten_list(file_extensions)
 
-                            file_path = file_path_from + '/' + file_name
-                            while os.path\
-                                    .exists(file_path_to + '/' + file_name):
-                                file_name = file_name[:file_name.rfind('.')]\
-                                            + '_'\
-                                            + str(duplicate)\
-                                            + file_name[file_name.rfind('.'):]
-                                duplicate += 1
+        # Get file count for the source folder
+        for _, _, files in os.walk(file_path_from):
+            n_files = len([file for file in files
+                           if file.endswith(tuple(file_extensions))])
+            break
 
-                            shutil.move(file_path,
-                                        file_path_to\
-                                        + '/'\
-                                        + file_name)
-                        elif file_name.endswith(ZIP_EXTENSIONS)\
-                            and root == file_path_from:
+        # Return if there are no files in the folder
+        if not n_files:
+            return -1
 
-                            for mydir in dirs:
-                                if mydir == file_name[:file_name.rfind('.zip')]\
-                                    or mydir == file_name[:file_name.rfind('.tar.gz')]\
-                                    or mydir == file_name[:file_name.rfind('.rar')]:
+        # Move files and remove archives that were already unzipped
+        for _, dirs, files in os.walk(file_path_from):
+            for file_name in files:
+                if file_name.endswith(tuple(file_extensions)):
+                    duplicate = 0
+                    file_name_dest = file_name
+                    while path.exists(path.join(file_path_to, file_name_dest)):
+                        duplicate += 1
+                        f_name, extension = path.splitext(file_name)
+                        file_name_dest = f'{f_name} ({duplicate}){extension}'
 
-                                    file_path = root + '/' + file_name
-                                    os.remove(file_path)
+                    shutil.move(path.join(file_path_from, file_name),
+                                path.join(file_path_to, file_name_dest))
+
+                if remove_zips:
+                    for dir_name in dirs:
+                        if dir_name == path.splitext(file_name):
+                            os.remove(path.join(file_path_from, file_name))
 
             completed = 0
             while completed <= 100:
                 completed += 100 / n_files
                 self.progress.setValue(completed)
+
+            # Break after the first execution so just the folders in
+            # current folder are moved
+            return n_files
 
 
 class MainFrame(QMainWindow):
@@ -112,9 +124,9 @@ class MainFrame(QMainWindow):
         self.setGeometry(50, 50, 700, 350)
         self.setWindowTitle('Mover - Move Your Files')
 
-        path = os.path.join(os.path.dirname(sys.modules[__name__].__file__),
-                            'logo.png')
-        self.setWindowIcon(QIcon(path))
+        logo_path = path.join(os.path.dirname(sys.modules[__name__].__file__),
+                              'logo.png')
+        self.setWindowIcon(QIcon(logo_path))
 
         # set style
         self.style_choice('cleanlooks')
@@ -149,10 +161,12 @@ class MainFrame(QMainWindow):
         # set browse buttons
         self.btn_browse_from = QPushButton('Browse...', self)
         self.btn_browse_to = QPushButton('Browse...', self)
+        self.btn_switch = QPushButton('Switch', self)
 
         # set action for browse buttons
         self.btn_browse_from.clicked.connect(self.file_open_le_from)
         self.btn_browse_to.clicked.connect(self.file_open_le_to)
+        self.btn_switch.clicked.connect(self.switch_paths)
 
         # set option checkboxes
         ckb_music_option = QCheckBox('Music files', self)
@@ -163,10 +177,10 @@ class MainFrame(QMainWindow):
                                    self)
 
         # set actions for checkboxes and create the list of options
-        self.checkedOptions = [MUSIC_EXTENSIONS,
-                               VIDEO_EXTENSIONS,
-                               PICTURES_EXTENSIONS,
-                               DOCUMENTS_EXTENSIONS]
+        self.checked_options = [MUSIC_EXTENSIONS,
+                                VIDEO_EXTENSIONS,
+                                PICTURES_EXTENSIONS,
+                                DOCUMENTS_EXTENSIONS]
 
         ckb_music_option.setChecked(True)
         ckb_music_option.stateChanged\
@@ -221,12 +235,15 @@ class MainFrame(QMainWindow):
 
         box_layout_horizontal = QHBoxLayout()
         box_layout_horizontal.addWidget(self.btn_move_files)
+        box_layout_horizontal_switch = QHBoxLayout()
+        box_layout_horizontal_switch.addWidget(self.btn_switch)
 
         # add the form, move files button and progress bar to a box layout
         box_layout = QVBoxLayout()
         box_layout.addLayout(form_layout)
         box_layout.addStretch()
         box_layout.addLayout(box_layout_horizontal)
+        box_layout.addLayout(box_layout_horizontal_switch)
         box_layout.addWidget(self.progress)
 
         # add the box layout containing all the components to a central widget
@@ -236,6 +253,41 @@ class MainFrame(QMainWindow):
         # set the central widget as the layout of the window
         self.setCentralWidget(central_widget)
 
+        # Set the defaults from cache file
+        if path.exists('.cachedpaths'):
+            with open('.cachedpaths', 'r') as cache:
+                lines = cache.readlines()
+                self.le_from.setText(lines[0].strip())
+                self.le_to.setText(lines[1].strip())
+
+                # Get a list of tuples of all the options from the cache
+                options = [tuple(line[1:-2].replace("'", '').split(', '))\
+                           for line in lines[2:]]
+                if MUSIC_EXTENSIONS in options:
+                    ckb_music_option.setChecked(True)
+                else:
+                    ckb_music_option.setChecked(False)
+
+                if DOCUMENTS_EXTENSIONS in options:
+                    ckb_doc_option.setChecked(True)
+                else:
+                    ckb_doc_option.setChecked(False)
+
+                if PICTURES_EXTENSIONS in options:
+                    ckb_pictures_option.setChecked(True)
+                else:
+                    ckb_pictures_option.setChecked(False)
+
+                if VIDEO_EXTENSIONS in options:
+                    ckb_video_option.setChecked(True)
+                else:
+                    ckb_video_option.setChecked(False)
+
+                if ZIP_EXTENSIONS in options:
+                    ckb_zip_option.setChecked(True)
+                else:
+                    ckb_zip_option.setChecked(False)
+
         self.show()
 
     # checkbox state changed
@@ -243,65 +295,98 @@ class MainFrame(QMainWindow):
         """ Add to the options list the checked checkboxes """
         if checkbox.text() == "Music files":
             if checkbox.isChecked():
-                self.checkedOptions.append(MUSIC_EXTENSIONS)
+                self.checked_options.append(MUSIC_EXTENSIONS)
             else:
-                self.checkedOptions.remove(MUSIC_EXTENSIONS)
+                self.checked_options.remove(MUSIC_EXTENSIONS)
 
         if checkbox.text() == "Text files":
             if checkbox.isChecked():
-                self.checkedOptions.append(DOCUMENTS_EXTENSIONS)
+                self.checked_options.append(DOCUMENTS_EXTENSIONS)
             else:
-                self.checkedOptions.remove(DOCUMENTS_EXTENSIONS)
+                self.checked_options.remove(DOCUMENTS_EXTENSIONS)
 
         if checkbox.text() == "Video files":
             if checkbox.isChecked():
-                self.checkedOptions.append(VIDEO_EXTENSIONS)
+                self.checked_options.append(VIDEO_EXTENSIONS)
             else:
-                self.checkedOptions.remove(VIDEO_EXTENSIONS)
+                self.checked_options.remove(VIDEO_EXTENSIONS)
 
         if checkbox.text() == "Image files":
             if checkbox.isChecked():
-                self.checkedOptions.append(PICTURES_EXTENSIONS)
+                self.checked_options.append(PICTURES_EXTENSIONS)
             else:
-                self.checkedOptions.remove(PICTURES_EXTENSIONS)
+                self.checked_options.remove(PICTURES_EXTENSIONS)
 
         if checkbox.text() == "Remove zip files that are already extracted":
             if checkbox.isChecked():
-                self.checkedOptions.append(ZIP_EXTENSIONS)
+                self.checked_options.append(ZIP_EXTENSIONS)
             else:
-                self.checkedOptions.remove(ZIP_EXTENSIONS)
+                self.checked_options.remove(ZIP_EXTENSIONS)
 
-    # open file
     def file_open_le_from(self):
         """ Open the directory browser to select the from directory """
         directory_path = QFileDialog.getExistingDirectory(self,
                                                           'Select directory')
         self.le_from.setText(directory_path)
 
-    # open file
     def file_open_le_to(self):
         """ Open the directory browser to select the to directory """
         directory_path = QFileDialog.getExistingDirectory(self,
                                                           'Select directory')
         self.le_to.setText(directory_path)
 
-    # progress bar function
+    def switch_paths(self):
+        """ Switch from and to paths """
+        path_from = self.le_from.text()
+        path_to = self.le_to.text()
+
+        self.le_from.setText(path_to)
+        self.le_to.setText(path_from)
+
+    @staticmethod
+    def cache_paths(path_from, path_to, checkbox_options):
+        """Save current configuration and make the file hidden"""
+        if path.exists('.cachedpaths'):
+            os.remove('.cachedpaths')
+
+        with open('.cachedpaths', 'w') as cache:
+            cache.write(f'{path_from}\n{path_to}\n')
+            for options in checkbox_options:
+                cache.write(f'{options}\n')
+
+        if os.name == 'nt':
+            check_call(['attrib', '+H', '.cachedpaths'])
+
     def move_files(self):
         """ Move the files and give an alert to get user's approval """
         # show a warning before moving files
         choice = QMessageBox.question(self,
                                       'Warning!',
-                                      'Are you sure you want to move this files?',
+                                      'Are you sure you want to move these files?',
                                       QMessageBox.Yes | QMessageBox.No)
         if choice == QMessageBox.Yes:
-            mover = Mover(self.progress)
-            mover.run_move_files(self.le_from.text(),
-                                self.le_to.text(),
-                                self.checkedOptions)
-        else:
-            pass
+            self.progress.reset()
+            # Save paths to cache
+            MainFrame.cache_paths(self.le_from.text(),
+                                  self.le_to.text(),
+                                  self.checked_options)
 
-    # close application
+            # Move the files
+            mover = Mover(self.progress)
+            result = mover.run_move_files(self.le_from.text(),
+                                          self.le_to.text(),
+                                          self.checked_options)
+
+            if result > 0:
+                self.statusBar()\
+                    .showMessage(f'Done. {result} files have been moved.')
+            elif result == -1:
+                self.statusBar()\
+                    .showMessage('No files to move.')
+            else:
+                self.statusBar()\
+                    .showMessage('Something went wrong. Please try again.')
+
     def close_application(self):
         """ Close the app """
         # Warning message
@@ -311,10 +396,7 @@ class MainFrame(QMainWindow):
                                       QMessageBox.Yes | QMessageBox.No)
         if choice == QMessageBox.Yes:
             sys.exit()
-        else:
-            pass
 
-    # choose style
     @staticmethod
     def style_choice(text):
         """ Set the style provided as a parameter """
